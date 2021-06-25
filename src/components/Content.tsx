d0fa31a278170c0503fcb6833e03e4d3488fbda0
Igor S. Silvano <59940906+igorsvn@users.noreply.github.com>
2021-06-25 16:37:10 -0300

    init

---
+++ src/components/Content.tsx
@@ -0,0 +1,46 @@
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenreTitle: string;
+}
+
+export function Content({ movies, selectedGenreTitle }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:
+          <span> {selectedGenreTitle}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard 
+              key ={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
